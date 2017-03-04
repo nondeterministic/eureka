29df3b2eee9177ff2ccd45e3b0b35c63841f2a8a
Andreas Bauer <baueran@gmail.com>
2017-03-04 19:42:04 +0100

    Oops

---
+++ data/Demoworld/edibles/garlic.lua
@@ -0,0 +1,20 @@
+-- -----------------------------------------------
+-- Edibles definition file for eureka
+-- Copyright (c) Andreas Bauer <baueran@gmail.com>
+-- -----------------------------------------------
+
+Edibles["garlic"]  = { 
+   name          = "garlic", 
+   plural_name   = "garlics", 
+
+   food_up                = 0,
+   healing_power          = "none",
+   poison_healing_power   = "none",
+   poisonous              = "none",
+   magic_herb             = true,
+   intoxicating_effect    = "none",
+
+   gold          = 1,
+   weight        = 0,
+   icon          = 988
+}
---
+++ data/Demoworld/edibles/sulphur.lua
@@ -0,0 +1,20 @@
+-- -----------------------------------------------
+-- Edibles definition file for eureka
+-- Copyright (c) Andreas Bauer <baueran@gmail.com>
+-- -----------------------------------------------
+
+Edibles["sulphur"]  = { 
+   name          = "sulphur", 
+   plural_name   = "sulphur", 
+
+   food_up                = 0,
+   healing_power          = "none",
+   poison_healing_power   = "very_little",
+   poisonous              = "some",
+   magic_herb             = true,
+   intoxicating_effect    = "none",
+
+   gold          = 1,
+   weight        = 0,
+   icon          = 988
+}
