eb3004238890a70d23c11c36065a4712abbb97a8
Andreas Bauer <baueran@bruckner>
2018-01-21 13:25:04 +0100

    Some world changes.

---
+++ data/Demoworld/weapons/pick.lua
@@ -0,0 +1,19 @@
+-- -----------------------------------------------
+-- Definition file for eureka
+-- Copyright (c) Andreas Bauer <baueran@gmail.com>
+-- -----------------------------------------------
+
+Weapons["pick"]  = { 
+   name         = "pick", 
+   plural_name  = "pick", 
+   hands        = 2, 
+   range        = 10,
+   damage_min   = 1,
+   damage_max   = 8,
+   damage_bonus = 0,
+   weight       = 1,
+   icon         = 412,
+   gold         = 1,
+   light_radius = 0,
+   destroy_after = 0 
+}
---
+++ data/Demoworld/weapons/shovel.lua
@@ -0,0 +1,19 @@
+-- -----------------------------------------------
+-- Definition file for eureka
+-- Copyright (c) Andreas Bauer <baueran@gmail.com>
+-- -----------------------------------------------
+
+Weapons["shovel"]  = { 
+   name         = "shovel", 
+   plural_name  = "shovels", 
+   hands        = 2, 
+   range        = 10,
+   damage_min   = 1,
+   damage_max   = 5,
+   damage_bonus = 0,
+   weight       = 1,
+   icon         = 411,
+   gold         = 1,
+   light_radius = 0,
+   destroy_after = 0 
+}
