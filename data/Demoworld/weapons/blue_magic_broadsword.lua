bba9cce75119d342761b4052a4e11cb14d5f5fca
Andreas Bauer <baueran@gmail.com>
2018-04-01 19:39:19 +0200

    Add item.

---
+++ data/Demoworld/weapons/blue_magic_broadsword.lua
@@ -0,0 +1,19 @@
+-- -----------------------------------------------
+-- Definition file for eureka
+-- Copyright (c) Andreas Bauer <baueran@gmail.com>
+-- -----------------------------------------------
+
+Weapons["blue magic broadsword"]  = { 
+   name         = "blue magic broadsword", 
+   plural_name  = "blue magic broadswords", 
+   hands        = 2, 
+   range        = 10,
+   damage_min   = 8,
+   damage_max   = 25,
+   damage_bonus = 5,
+   weight       = 1,
+   icon         = 616,
+   gold         = 300,
+   light_radius = 4,
+   destroy_after = 0 
+}
